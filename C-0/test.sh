f936927f345aac682a3fb8219fa031ca7ebb35d9
Raspi <twawrik@yahoo.de>
2022-06-24 18:48:24 +0200

    Better visualization when using test.sh

--- C-0/test.sh
+++ C-0/test.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+LightRed='\033[1;31m';
+LightGray='\033[0;37m';
+
 for folder in `ls -d */`
 do
 	echo $folder
-	for file in `ls $folder/*/*.in`
+	echo "\tWe expect ${LightRed}1${LightGray} here."
+	for file in `ls ${folder}/SUCCEEDING/`
 	do
-		me=`./a.out 1 $file | grep -F -c 'successful'`
-		echo "\t$file: $me" ;
+		me=`./a.out 1 $folder/SUCCEEDING/$file | grep -F -c 'successful'`
+		echo "\t\t$file: $me" ;
+	done
+	echo "\tWe expect ${LightRed}0${LightGray} here."
+	for file in `ls ${folder}/FAILING/`
+	do
+		me=`./a.out 1 $folder/FAILING/$file | grep -F -c 'successful'`
+		echo "\t\t$file: $me" ;
 	done
 done
+
+echo ""
+echo "${LightRed}Unexpected results? Did you run 'make'?";
