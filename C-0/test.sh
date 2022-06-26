0e03f2b39434587500a9f5f203cca1da1de5763d
Raspi <twawrik@yahoo.de>
2022-06-26 14:42:59 +0200

    Changed test.sh. Should return 1 now for every test. This way, we can detect 'Segmentation Fault's too.

--- C-0/test.sh
+++ C-0/test.sh
@@ -3,20 +3,20 @@
 LightRed='\033[1;31m';
 LightGray='\033[0;37m';
 
+echo "\tWe expect ${LightRed}1${LightGray} on every result."
+
 for folder in `ls -d */`
 do
 	echo $folder
-	echo "\tWe expect ${LightRed}1${LightGray} here."
 	for file in `ls ${folder}/SUCCEEDING/`
 	do
 		me=`./a.out 1 $folder/SUCCEEDING/$file | grep -F -c 'successful'`
-		echo "\t\t$file: $me" ;
+		echo "\t\t/SUCCEEDING/$file: $me" ;
 	done
-	echo "\tWe expect ${LightRed}0${LightGray} here."
 	for file in `ls ${folder}/FAILING/`
 	do
-		me=`./a.out 1 $folder/FAILING/$file | grep -F -c 'successful'`
-		echo "\t\t$file: $me" ;
+		me=`./a.out 1 $folder/FAILING/$file | grep -F -c 'ERROR syntax error'`
+		echo "\t\t/FAILING/$file: $me" ;
 	done
 done
 
